a11132c6cd67a94099a34363a84e0aa8b79e6d27
ZABILA PETRO <petrozabila@gmail.com>
2022-08-01 17:01:52 +0300

    fixes to ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,6 +35,7 @@ export default class ContactForm extends Component {
                 <input
                     type="text"
                     name="name"
+                    value={this.state.name}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     onChange={this.onInputChange}
@@ -45,6 +46,7 @@ export default class ContactForm extends Component {
                 <input
                         type="tel"
                         name="number"
+                        value={this.state.number}
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         onChange={this.onInputChange}
